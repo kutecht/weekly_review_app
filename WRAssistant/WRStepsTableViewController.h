ccb2b93967e2d4e74478b2af0e8a96e054fa2d72
Kevin Utecht <kevin@kutecht.com>
2013-08-11 18:37:42 -0500

    Start at getting iPad screens defined.

---
+++ WRAssistant/WRStepsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  WRStepsTableViewController.h
+//  WRAssistant
+//
+//  Created by Kevin Utecht on 8/11/13.
+//  Copyright (c) 2013 kevin utecht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WRStepsTableViewController : UITableViewController
+
+@end
